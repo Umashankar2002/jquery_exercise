1e519a7dff3997a9e3af5600507a7db037355991
Umashankar2002 <r180987@rguktrkv.ac.in>
2025-09-09 11:26:44 +0530

    Chenged background colors and borders

--- jquery.js
+++ jquery.js
@@ -1,14 +1,13 @@
-
-$(document).ready(function(){
+$(document).ready(function() {
 
     //changing the background color for all sections
     $(".section-bg").addClass("highlight-bg");
 
+    //removing the border for all input elements
+    $(".input-field").addClass("input-field-border");
+
     //changing the border color #FF1A00 and adding borders to some sections
     $(".section-border").addClass("highlight-border");
-
-     //hiding footer section of the resume
-    $("#mypage-footer").hide();
 
     //changing the font color to #CC0000
     $(".section-text").addClass("highlight-text");
@@ -16,10 +15,10 @@ $(document).ready(function(){
     //changing the font color for button text to #CC0000
     $(".button").addClass("button-bg");
 
-    //removing the border for all input elements
-    $(".input-field").addClass("input-field-border");
+     //hiding footer section of the resume
+    $("#mypage-footer").hide();
 
     // showing the reference section
-    $("#reference-section").show();
+    $("#reference").show();
 
 })
